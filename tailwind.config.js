f26478b892ce8799e7d0746ccbc058642308b342
Ali <108173040+its4li@users.noreply.github.com>
2025-07-27 16:54:06 +0330

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#0D0D0D", // مشکی تیره
+        primary: "#8B5CF6",     // بنفش
+        muted: "#AAAAAA",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
